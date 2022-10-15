abf2fb0acca440375023ffe9e0079cdf8a5e81bf
Kyle Dell <kdell1@umbc.edu>
2022-10-15 17:34:44 -0400

    got messaging functionality working for tcp

--- ParentProcess.c
+++ ParentProcess.c
@@ -7,12 +7,38 @@
 #include <string.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-#define PORT 9765
+#define PORT 9756
 #define HELLO "Hello from Server\n"
 
+
 int likeServerSend(int likes, int id) {
-    char 
-    printf("");
+    char msg[20];
+    sprintf(msg, "LikeServer%d %02d\n", id, likes);
+    #ifdef DEBUG
+    printf("%s\n", msg);
+    #endif
+    int sfd, valread, cfd;
+    char buffer[1024] = { 0 };
+    struct sockaddr_in address = {0};
+    address.sin_family = AF_INET;
+    address.sin_port = htons(PORT);
+    socklen_t server_info_len = sizeof(address);
+
+    if ((sfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket Creation Failed");
+        exit(1);
+    }
+
+    if ((cfd = connect(sfd, (struct sockaddr*)&address, server_info_len)) < 0) {
+        perror("Connection to Server Failed");
+        return -1;
+    }
+
+    send(sfd, msg, strlen(msg), 0);
+    printf("Sent the message: %s", msg);
+    valread = read(sfd, buffer, 1024);
+    printf("%s\n", buffer);
+
     return 0;
 }
 
@@ -23,26 +49,40 @@ void likeServer(int id){
     time_t start_t, end_t;
     time(&start_t);
     time(&end_t);
-    while(difftime(end_t, start_t) > 300) {
+    sleep(2);
+
+    // run the server until it hits 5 minutes
+    while (difftime(end_t, start_t) <= 300) {
+        #ifdef DEBUG
+        printf("Random sleep time: %d\n", sleepTime);
+        printf("Hello from the LikeServer%d: The Random Number generated was %d\n", id, likes);
+        #endif
         sleep(sleepTime);
         if (likeServerSend(likes, id) > 0) {
             likes = rand() % 42;
         }
         time(&end_t);
     }
-    printf("Hello from the LikeServer%d: The Random Number generated was %d\n", id, random);
+
     exit(0);
 }
 
 void primaryLikeServer() {
+    #ifdef DEBUG
     printf("Hello from the primary like server\n");
+    #endif
     // declare the variables for socket programming
     int sfd, cfd, valread;
+    int opt = 1;
     char buffer[1024] = { 0 };
     struct sockaddr_in address = { 0 };
     address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
     socklen_t server_info_len = sizeof(address);
+    time_t start_t, end_t;
+    time(&start_t);
+    time(&end_t);
 
     // Initialize the socket using IPv4 and TCP
     if ((sfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
@@ -50,9 +90,14 @@ void primaryLikeServer() {
         exit(1);
     }
 
+    if (setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0) {
+        perror("Set Socket Error");
+        exit(1);
+    }
+
     // bind on the 9765 port number
     if (bind(sfd, (struct sockaddr*) &address, server_info_len) < 0) {
-        perror("Error binding to to port");
+        perror("Error binding to port");
         exit(1);
     }
 
@@ -62,18 +107,20 @@ void primaryLikeServer() {
         exit(1);
     }
 
-    // accept the connection and keep the socket info for the client to send a response
-    if ((cfd = accept(sfd, (struct sockaddr*) &address, (socklen_t*)&server_info_len)) < 0) {
-        perror("Error while accepting connection");
-        exit(1);
+    while (difftime(end_t, start_t) <= 360) {
+        // accept the connection and keep the socket info for the client to send a response
+        if ((cfd = accept(sfd, (struct sockaddr*) &address, (socklen_t*)&server_info_len)) < 0) {
+            perror("Error while accepting connection");
+            exit(1);
+        }
+
+        // read the buffer then print and reply with the hello message
+        valread = read(cfd, buffer, 1024);
+        printf("Server Received message: %s\n", buffer);
+        send(cfd, HELLO, strlen(HELLO), 0);
+        close(cfd);         
     }
-
-    // read the buffer then print and reply with the hello message
-    valread = read(cfd, buffer, 1024);
-    printf("%s\n", buffer);
-    send(cfd, HELLO, strlen(HELLO), 0);
-    close(cfd);
-
+    
     exit(0);
 }
 
@@ -85,11 +132,13 @@ int main() {
         pid[ii] = fork();
         if (pid[ii] == 0) {
             id = ii;
+            #ifdef DEBUG
             if (ii < 10) {
                 printf("LikeServer%d is running\n", id);
             } else {
                 printf("PrimaryLikeServer is Alive\n");
             }
+            #endif
             ii = 100;
         } else if (pid[ii] > 0) {
             // print out that the like servers have been started
@@ -117,18 +166,7 @@ int main() {
                     fprintf(filePtr, "LikeServer%d did not start properly\n", ii);
                 } else {
                     fprintf(filePtr, "The PrimaryLikeServer did not start properly");
-                }FILE *filePtr = NULL;
-            filePtr = fopen("/tmp/ParentProcessStatus", "a");
-            if (filePtr == NULL) {
-                perror("Error Opening Parent Process Log File");
-            } else {
-                if (ii < 10) {
-                    fprintf(filePtr, "The Child Process for LikeServer%d was started with PID %d\n", ii, pid);
-                } else {
-                    fprintf(filePtr, "The Child Process for PrimaryLikeServer was started with PID %d\n", pid);
                 }
-                fclose(filePtr);
-            }
                 fclose(filePtr);
             }
         }
@@ -145,7 +183,9 @@ int main() {
         for (int ii = 0; ii < 11; ii++) {
             pid_t tempPid = waitpid(pid[ii], &stat, 0);
             if (WIFEXITED(stat)) {
+                #ifdef DEBUG
                 printf("LikeServer%d terminated with status: %d\n", ii, WEXITSTATUS(stat));
+                #endif
             }
         }
     }
